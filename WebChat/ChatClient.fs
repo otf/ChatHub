d80bdcc45a09c8a23d1df7c4448d6b79fba03498
otf <phoenixsingleton@gmail.com>
2014-08-17 00:45:38 +0900

    メッセージ追加機能を追加

--- WebChat/ChatClient.fs
+++ WebChat/ChatClient.fs
@@ -2,13 +2,24 @@ namespace WebChat
 
 open IntelliFactory.WebSharper
 open IntelliFactory.WebSharper.Html
+open IntelliFactory.WebSharper.JQuery
 
 [<JavaScript>]
 module ChatClient =
-    let render =
+    let renderMessage (msg : string) =
+        Div [Text msg]
+
+    let sendMessage (msgBox : Element) =
+        let msgElm = renderMessage msgBox.Value
+        ById("chat-box").AppendChild(msgElm.Dom) |> ignore
+        msgBox.Value <- ""
+       
+    let renderMain =
+        let msgBox = Input [ Text ""; Attr.Id "message-box"; Attr.Type "text" ] 
         Div [
             Div [ Attr.Id "chat-box" ]
-            Input [ Text ""; Attr.Id "message-box"; Attr.Type "text" ] 
+            msgBox
+            |>! OnKeyPress (fun input char -> if char.CharacterCode = 13 then sendMessage msgBox)
         ]
 
 type ChatControl () =
@@ -16,4 +27,4 @@ type ChatControl () =
 
     [<JavaScript>]
     override __.Body =
-        ChatClient.render :> _
+        ChatClient.renderMain :> _
