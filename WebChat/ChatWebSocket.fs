9d59b108eec4166032b32a0340944b6e40a8b465
otf <phoenixsingleton@gmail.com>
2014-08-24 02:19:17 +0900

    Client型を削除し、Dictionaryに直接チャット相手のHandlerを突っ込むようにした

--- WebChat/ChatWebSocket.fs
+++ WebChat/ChatWebSocket.fs
@@ -6,37 +6,29 @@ open System.Collections.Generic
 
 open Protocol
 
-type Client<'a> = 
-    | Waiting
-    | Chatting of 'a
-
 type WebSocketChatHandler () =
     inherit WebSocketHandler ()
 
-    static let handlerClientMap = Dictionary()
+    static let chats = Dictionary()
     static let waitingList = ResizeArray()
 
     let addAndTryBeginChat (me : WebSocketChatHandler) = 
         match waitingList |> Seq.tryFind(fun _ -> true) with
         | Some other -> 
-            handlerClientMap.[other] <- Chatting me
-            handlerClientMap.[me] <- Chatting other
+            chats.[other] <- me
+            chats.[me] <- other
             Join |> encodeServerProtocol |> other.Send
             Join |> encodeServerProtocol |> me.Send
             waitingList.Remove(other) |> ignore
         | _ -> 
-            handlerClientMap.[me] <- Waiting
             waitingList.Add(me)
     
     let tryRemove (me : WebSocketChatHandler) =
-        waitingList.Remove(me) |> ignore
-        match handlerClientMap.TryGetValue(me) with
-        | (true, Chatting other) -> 
-            other.Close()
-            handlerClientMap.Remove(other) |> ignore
-        | _ -> ()
-
-        handlerClientMap.Remove(me) |> ignore
+        if not <| waitingList.Remove(me) then
+            let other = chats.[me]
+            chats.[me].Close()
+            chats.Remove(me) |> ignore
+            chats.Remove(other) |> ignore
 
     let tryInputting other =
         Written |> encodeServerProtocol |> (other : WebSocketChatHandler).Send
@@ -45,12 +37,12 @@ type WebSocketChatHandler () =
         Listen msg |> encodeServerProtocol |> (other : WebSocketChatHandler).Send
 
     let brancketMe this f =
-        lock handlerClientMap (fun () -> f this)
+        lock chats (fun () -> f this)
 
     let brancketOther this f =
-        lock handlerClientMap (fun () ->
-            match handlerClientMap.TryGetValue(this) with
-            | (true, Chatting other) ->
+        lock chats (fun () ->
+            match chats.TryGetValue(this) with
+            | (true, other) ->
                 f other
             | _ ->
                 failwith "チャット中じゃないのに発言しようとしました。")
